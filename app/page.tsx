1896d8e4fb4c934fdec9bd252b6ef61230ac2428
beatsmeidk <tamam.elalti@gmail.com>
2026-09-21 19:01:16 +0300

    Revert "Add photographic service overview with accessible hover animations"
    This reverts commit 66201e6ec63d75212f7383f7b08f7b91166af123.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import ServiceOverview from '@/components/campaign/ServiceOverview';
 import MaterialExplorer from '@/components/campaign/MaterialExplorer';
 import HomeCare from '@/components/campaign/HomeCare';
-export default function Home(){return <main id="main" className="life-home"><MaterialExplorer/><ServiceOverview/><HomeCare/></main>}
+export default function Home(){return <main id="main" className="life-home"><MaterialExplorer/><HomeCare/></main>}
